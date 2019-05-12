3bda9b9d9b36fcbe817dc3d19aa697de9efc02ae
dennisschroeder <dennisschroeder@me.com>
2019-05-12 03:11:22 +0200

    [CHANGES] Adds another abstraction layer on top of the EntityInterface

---
+++ src/main/kotlin/khome/core/entities/AbstractEntity.kt
@@ -0,0 +1,23 @@
+package khome.core.entities
+
+import khome.core.State
+import khome.listening.getState
+import khome.listening.getStateAttributes
+import khome.listening.exceptions.EntityStateNotFoundException
+import khome.listening.exceptions.EntityStateAttributeNotFoundException
+
+abstract class AbstractEntity(entityDomain: String, entityName: String) : EntityInterface {
+    override val domain: String = entityDomain
+    override val name: String = entityName
+    override val entityId: String get() = "$domain.$name"
+    override val state: State by lazy { getState(entityId) }
+    override val attributes: Map<String, Any> by lazy { getStateAttributes(entityId) }
+
+    inline fun <reified StateValueType> getStateValue(name: String): StateValueType =
+        Sun.state.getValue<StateValueType>()
+            ?: throw throw EntityStateNotFoundException("No state for entity with id: $entityId found.")
+
+    inline fun <reified AttributeValueType> getAttributeValue(name: String): AttributeValueType =
+        Sun.state.getAttribute(name)
+            ?: throw EntityStateAttributeNotFoundException("No state attribute for entity with name: ${Sun.entityId} and name: $name found.")
+}
--- src/main/kotlin/khome/core/entities/Sun.kt
+++ src/main/kotlin/khome/core/entities/Sun.kt
@@ -1,18 +1,3 @@
 package khome.core.entities
 
-import khome.core.State
-import khome.listening.exceptions.EntityStateAttributeNotFoundException
-import khome.listening.getState
-import khome.listening.getStateAttributes
-
-object Sun : EntityInterface {
-    override val domain: String = "sun"
-    override val name: String = "sun"
-    override val entityId: String = "$domain.$name"
-    override val state: State by lazy { getState(entityId) }
-    override val attributes: Map<String, Any> by lazy { getStateAttributes(entityId) }
-
-    inline fun <reified AttributeValueType> getAttributeValue(name: String): AttributeValueType =
-        Sun.state.getAttribute(name)
-            ?: throw EntityStateAttributeNotFoundException("No state attribute for entity with name: $entityId and name: $name found.")
-}
+object Sun : AbstractEntity("sun", "sun")
