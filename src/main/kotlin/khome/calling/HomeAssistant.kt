1ed00c66acfeb63a002d3c35514a2c7c97b77b4c
dennisschroeder <dennisschroeder@me.com>
2019-05-12 03:44:54 +0200

    [TASK] Adds classes referencing to domain homeassistant functions

--- src/main/kotlin/khome/calling/HomeAssistant.kt
+++ src/main/kotlin/khome/calling/HomeAssistant.kt
@@ -1,9 +1,18 @@
 package khome.calling
 
+import khome.core.entities.EntityInterface
+import khome.listening.getEntityInstance
+
 fun ServiceCaller.turnOn(entityId: String) {
     domain = "homeassistant"
     service = "turn_on"
     serviceData = EntityId(entityId)
+}
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.turnOn() {
+    val entity = getEntityInstance<Entity>()
+
+    turnOn(entity.entityId)
 }
 
 fun ServiceCaller.turnOff(entityId: String) {
@@ -12,16 +21,34 @@ fun ServiceCaller.turnOff(entityId: String) {
     serviceData = EntityId(entityId)
 }
 
+inline fun <reified Entity : EntityInterface> ServiceCaller.turnOff() {
+    val entity = getEntityInstance<Entity>()
+
+    turnOff(entity.entityId)
+}
+
 fun ServiceCaller.toggle(entityId: String) {
     domain = "homeassistant"
     service = "toggle"
     serviceData = EntityId(entityId)
 }
 
+inline fun <reified Entity : EntityInterface> ServiceCaller.toggle() {
+    val entity = getEntityInstance<Entity>()
+
+    toggle(entity.entityId)
+}
+
 fun ServiceCaller.updateEntity(entityId: String) {
     domain = "homeassistant"
     service = "update_entity"
     serviceData = EntityId(entityId)
+}
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.updateEntity() {
+    val entity = getEntityInstance<Entity>()
+
+    updateEntity(entity.entityId)
 }
 
 fun ServiceCaller.updateEntities(vararg entityIds: String) {
