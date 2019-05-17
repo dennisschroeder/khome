35d77cbdfc756cfa0488c0efd433831fabd0c6f8
dennisschroeder <dennisschroeder@me.com>
2019-05-17 10:11:02 +0200

    [TASK] Adds higher level abstraction cover functions

--- src/main/kotlin/khome/calling/Cover.kt
+++ src/main/kotlin/khome/calling/Cover.kt
@@ -1,6 +1,45 @@
 package khome.calling
 
+import khome.listening.getEntityInstance
+import khome.core.entities.EntityInterface
 import com.google.gson.annotations.SerializedName
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.openCover() {
+    val entity = getEntityInstance<Entity>()
+    openCover(entity.entityId)
+}
+
+fun ServiceCaller.openCover(entityId: String) {
+    cover {
+        this.entityId = entityId
+        service = openCover
+    }
+}
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.closeCover() {
+    val entity = getEntityInstance<Entity>()
+    closeCover(entity.entityId)
+}
+
+fun ServiceCaller.closeCover(entityId: String) {
+    cover {
+        this.entityId = entityId
+        service = closeCover
+    }
+}
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.setCoverPositionTo(position: Int) {
+    val entity = getEntityInstance<Entity>()
+    setCoverPositionTo(position, entity.entityId)
+}
+
+fun ServiceCaller.setCoverPositionTo(position: Int, entityId: String) {
+    cover {
+        this.entityId = entityId
+        service = setCoverPosition
+        this.position = position
+    }
+}
 
 fun ServiceCaller.cover(init: CoverData.() -> Unit) {
     domain = "cover"
