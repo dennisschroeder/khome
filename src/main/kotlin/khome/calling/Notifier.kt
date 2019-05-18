b5a67cb85cf2afa7cc480d1375641488ebd52aad
dennisschroeder <dennisschroeder@me.com>
2019-05-18 17:26:28 +0200

    [TASK] Adds notifyIOS feature

--- src/main/kotlin/khome/calling/Notifier.kt
+++ src/main/kotlin/khome/calling/Notifier.kt
@@ -1,6 +1,24 @@
 package khome.calling
 
+import khome.listening.getEntityInstance
+import khome.core.entities.EntityInterface
 import com.google.gson.annotations.SerializedName
+
+inline fun <reified Entity : EntityInterface> ServiceCaller.notifyIOS(noinline init: NotifierMessage.() -> Unit) {
+    val entity = getEntityInstance<Entity>()
+    notifyIOS(entity.name, init)
+}
+
+fun ServiceCaller.notifyIOS(deviceId: String, init: NotifierMessage.() -> Unit) {
+    domain = "notify"
+    service = "ios_$deviceId"
+    serviceData = NotifierMessage(
+        null,
+        null,
+        null,
+        null
+    ).apply(init)
+}
 
 fun ServiceCaller.notify(init: NotifierMessage.() -> Unit) {
     domain = "notify"
