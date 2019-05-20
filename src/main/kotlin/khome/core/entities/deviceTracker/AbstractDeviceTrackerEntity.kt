89f603dc6d4a1a5a259465ffebf8cd61bc2abbd6
dennisschroeder <dennisschroeder@me.com>
2019-05-20 11:14:38 +0200

    [TASK] Adds abstract NotifierIOS Service val

--- src/main/kotlin/khome/core/entities/deviceTracker/AbstractDeviceTrackerEntity.kt
+++ src/main/kotlin/khome/core/entities/deviceTracker/AbstractDeviceTrackerEntity.kt
@@ -1,8 +1,11 @@
 package khome.core.entities.deviceTracker
 
+import khome.calling.ServiceInterface
 import khome.core.entities.AbstractEntity
 
-abstract class AbstractDeviceTrackerEntity(deviceEntityName: String) : AbstractEntity("device_tracker", deviceEntityName) {
+abstract class AbstractDeviceTrackerEntity(deviceEntityName: String) :
+    AbstractEntity("device_tracker", deviceEntityName) {
     val isHome = getStateValue<String>() == "home"
     val isAway = getStateValue<String>() == "not_home"
+    abstract val notifyIosService: ServiceInterface
 }
