99d82917cec095904f8d8617daca7a1029de805c
dennisschroeder <dennisschroeder@me.com>
2019-05-22 16:36:15 +0200

    [CHANGE] switched val assignment to getter version

--- src/main/kotlin/khome/core/entities/inputBoolean/AbstractBooleanEntity.kt
+++ src/main/kotlin/khome/core/entities/inputBoolean/AbstractBooleanEntity.kt
@@ -3,6 +3,6 @@ package khome.core.entities.inputBoolean
 import khome.core.entities.AbstractEntity
 
 abstract class AbstractBooleanEntity(inputBooleanEntityName: String) : AbstractEntity("input_boolean", inputBooleanEntityName) {
-    val isOn = getStateValue<String>() == "on"
-    val isOff = getStateValue<String>() == "off"
+    val isOn get() = getStateValue<String>() == "on"
+    val isOff get() = getStateValue<String>() == "off"
 }
