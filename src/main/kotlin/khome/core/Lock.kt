7d00653ca5cb033de1aea2cffa6abe0a3b0a4bfc
dennisschroeder <dennisschroeder@me.com>
2019-07-01 15:57:51 +0200

    [TASK] Adds function to unlock all entities (clear list)

--- src/main/kotlin/khome/core/Lock.kt
+++ src/main/kotlin/khome/core/Lock.kt
@@ -1,19 +1,22 @@
 package khome.core
 
-class Lock<T> {
-    private val list = mutableListOf<T>()
+import java.util.*
+
+open class Lock<T> {
+
+    private val list = Collections.synchronizedList(mutableListOf<T>())
 
     infix fun hasLocked(item: T): Boolean {
         return item in list
     }
 
-    @Synchronized
-    infix fun lock(item: T) {
+    fun lock(item: T) {
         list += item
     }
 
-    @Synchronized
-    infix fun unLock(item: T) {
+    fun unLock(item: T) {
         list -= item
     }
+
+    fun unLockAll() = list.clear()
 }
