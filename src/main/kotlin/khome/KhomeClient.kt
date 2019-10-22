645f1e27172968a2f48a8c77b4505c5f2b106630
Dennis Schröder <dennis.schroeder@cbc.de>
2019-10-22 14:25:49 +0200

    [CHANGE] - Wrapps ktor client into own class

---
+++ src/main/kotlin/khome/KhomeClient.kt
@@ -0,0 +1,26 @@
+package khome
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.websocket.DefaultClientWebSocketSession
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.ws
+import io.ktor.client.features.websocket.wss
+import io.ktor.http.HttpMethod
+import io.ktor.util.KtorExperimentalAPI
+import khome.core.ConfigurationInterface
+
+@KtorExperimentalAPI
+internal class KhomeClient(private val config: ConfigurationInterface) {
+
+    private val httpClient = HttpClient(CIO).config { install(WebSockets) }
+
+    private val method = HttpMethod.Get
+    private val path = "/api/websocket"
+    private val isSecure: Boolean = config.secure
+
+    suspend fun startSession(block: suspend DefaultClientWebSocketSession.() -> Unit) = when (isSecure) {
+        true -> httpClient.wss(method = method, host = config.host, port = config.port, path = path, block = block)
+        false -> httpClient.ws(method = method, host = config.host, port = config.port, path = path, block = block)
+    }
+}
