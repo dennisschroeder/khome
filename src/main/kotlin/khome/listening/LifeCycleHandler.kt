282ad721af344c65f760ae161d68621c69552401
Dennis Schröder <42579679+dennisschroeder@users.noreply.github.com>
2020-01-11 16:45:36 +0100

    feat: integrates scheduled tasks based on hass sensor.time entity (#28)

--- src/main/kotlin/khome/listening/LifeCycleHandler.kt
+++ src/main/kotlin/khome/listening/LifeCycleHandler.kt
@@ -1,10 +1,11 @@
 package khome.listening
 
 import io.ktor.util.KtorExperimentalAPI
-import khome.core.entities.EntityInterface
 import khome.core.LifeCycleHandlerInterface
 import khome.core.dependencyInjection.KhomeComponent
+import khome.core.entities.EntityInterface
 import khome.core.eventHandling.StateChangeEvent
+import khome.core.logger
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import org.koin.core.inject
 
@@ -16,5 +17,7 @@ class LifeCycleHandler(val handle: String, val entityId: EntityInterface) : Life
         TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
-    fun disable() = stateChangeEvent.unsubscribe(handle)
+    fun disable() = stateChangeEvent.unsubscribe(handle).also {
+        logger.info { "Callback with handle: \"$handle\" disabled." }
+    }
 }
