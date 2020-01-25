2de84464fa46e7a6e545ea7ee9c98c90cf6fc771
Dennis Schröder <dennis.schroeder@cbc.de>
2020-01-25 18:59:24 +0100

    feat: adds custom event class

--- src/main/kotlin/khome/core/eventHandling/CustomEvent.kt
+++ src/main/kotlin/khome/core/eventHandling/CustomEvent.kt
@@ -1,4 +1,26 @@
 package khome.core.eventHandling
 
-class CustomEvent {
+import khome.core.dependencyInjection.KhomeComponent
+import org.koin.core.get
+
+typealias EventData = Map<String, Any>
+
+abstract class CustomEvent(eventName: String, delegate: Event<EventData>) : KhomeComponent(), CustomEventInterface<EventData> {
+    override val eventType: String = eventName
+
+    init {
+        get<CustomEventRegistry>().register(eventType)
+    }
+
+    private val event = delegate
+    override val listenerCount get() = event.listeners.size
+    override fun subscribe(handle: String?, callback: EventData.() -> Unit) {
+        if (handle == null) event += callback else event[handle] = callback
+    }
+
+    override fun unsubscribe(handle: String) {
+        event -= handle
+    }
+
+    override fun emit(eventData: EventData) = event(eventData)
 }
