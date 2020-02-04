9678d3249f2b8980a8b7addeabd081d23f87b50b
Dennis Schröder <dennis.schroeder@cbc.de>
2020-02-04 08:46:48 +0100

    refactor: adds proper logging handling

--- src/main/kotlin/khome/listening/LifeCycleHandler.kt
+++ src/main/kotlin/khome/listening/LifeCycleHandler.kt
@@ -11,7 +11,7 @@ import org.koin.core.inject
 @KtorExperimentalAPI
 @ObsoleteCoroutinesApi
 class LifeCycleHandler(val handle: String) : LifeCycleHandlerInterface, KhomeComponent() {
-    private val logger = KotlinLogging.logger {  }
+    private val logger = KotlinLogging.logger { }
     private val stateChangeEvent: StateChangeEvent by inject()
     override fun cancel() {
         TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
