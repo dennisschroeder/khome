231f93d06f1b56fdd97736db47f4a59125494977
Dennis Schröder <dennis.schroeder@cbc.de>
2020-02-27 20:39:47 +0100

    fix: onTime with TimeEntity evaluates stateValue now lazy

--- src/main/kotlin/khome/core/entities/mediaPlayer/AbstractMediaPlayerEntity.kt
+++ src/main/kotlin/khome/core/entities/mediaPlayer/AbstractMediaPlayerEntity.kt
@@ -3,8 +3,8 @@ package khome.core.entities.mediaPlayer
 import khome.core.entities.AbstractEntity
 
 abstract class AbstractMediaPlayerEntity(name: String) : AbstractEntity<String>("media_player", name) {
-    val isOn  get() = stateValue == "on"
-    val isOff  get() = stateValue == "off"
+    val isOn get() = stateValue == "on"
+    val isOff get() = stateValue == "off"
     val isIdle get() = stateValue == "idle"
     val isMute get() = if (isOn) getAttributeValue("is_volume_muted") else false
     val volume get() = if (isOn) getAttributeValue("volume_level") else 0.00
