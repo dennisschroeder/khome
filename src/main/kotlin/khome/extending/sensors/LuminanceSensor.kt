0a191a7d357b12055e6b208a92c9a95db571175a
Dennis Schröder <dennis.schroeder@cbc.de>
2020-07-10 08:28:38 +0200

    feat: adds more sensors and docs

---
+++ src/main/kotlin/khome/extending/sensors/LuminanceSensor.kt
@@ -0,0 +1,2 @@
+package khome.extending.sensors
+
---
+++ src/main/kotlin/khome/extending/sensors/binary/MotionSensor.kt
@@ -0,0 +1,18 @@
+package khome.extending.sensors
+
+import khome.KhomeApplication
+import khome.entities.Attributes
+import khome.entities.devices.Sensor
+import khome.extending.SwitchableState
+import java.time.Instant
+
+typealias MotionSensor = Sensor<SwitchableState, MotionSensorAttributes>
+
+data class MotionSensorAttributes(
+    override val friendlyName: String,
+    override val lastChanged: Instant,
+    override val lastUpdated: Instant
+) : Attributes
+
+@Suppress("FunctionName")
+fun KhomeApplication.MotionSensor(objectId: String) : MotionSensor = BinarySensor(objectId)
